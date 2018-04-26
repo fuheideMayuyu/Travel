93330750f03b07c860efb0d2ad59b343cad0706a
fuheideMayuyu <xiaguangding@163.com>
2018-04-27 00:07:19 +0800

    banner finish

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <detail-banner></detail-banner>
+</template>
+
+<script type='text/ecmascript-6'>
+import DetailBanner from './components/banner'
+
+export default {
+  name: 'detail',
+  components: {
+    DetailBanner
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+</style>

d819527099bee27a82ed9e80845bcdefa1871728
fuheideMayuyu <xiaguangding@163.com>
2018-04-27 17:41:19 +0800

    commponents recursion

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,19 +2,41 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script type='text/ecmascript-6'>
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
 
 export default {
   name: 'detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票'
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>

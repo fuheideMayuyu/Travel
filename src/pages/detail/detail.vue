bbf342a35def4681032c40703cae8a1de0511f69
fuheideMayuyu <xiaguangding@163.com>
2018-04-27 15:05:50 +0800

    add detail-header

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,17 +1,25 @@
 <template>
-  <detail-banner></detail-banner>
+  <div>
+    <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
+  </div>
 </template>
 
 <script type='text/ecmascript-6'>
 import DetailBanner from './components/banner'
+import DetailHeader from './components/header'
 
 export default {
   name: 'detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang='stylus' scoped>
+.content
+  height 50rem
 </style>
